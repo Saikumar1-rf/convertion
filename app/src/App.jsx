66cffac75a377738bdc6eb646cab6023a06664ac
Saikumar1-rf <saikumar.rfchh@gmail.com>
2025-06-06 16:40:25 +0530

    builded

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,7 +3,7 @@ import ImageToExcelConverter from "./ImageToExcelConverter";
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 py-12">
-      <ImageToExcelConverter />
+      <ImageToExcelConverter /> 
     </div>
   );
 }
